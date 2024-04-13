16d112e78e5113c137b21f917a6dec2212f3db07
+ <+>
2024-04-13 12:38:28 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+// let users = {
+//   name: 'name',
+//   email: 'email',
+// };
+
+function submitData(name, email){
+  let users = {
+    name: name,
+    email: email,
+  }
+  return fetch("http://localhost:3000/users",{
+  method: 'POST',
+  headers: {
+  'Content-Type': 'application/json',
+  "Accept": "application/json",
+  },
+  body:JSON.stringify(users)
+})
+.then(res => res.json())
+.then(users => document.querySelector("body").append(users.id))
+.catch(error => document.querySelector("body").append(error.message))
+}
